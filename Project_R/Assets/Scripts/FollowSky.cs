91ea7de058ef7f58b7c140b4a21a4869cfc50a20
devblue_ <wooyeophan@gmail.com>
2020-08-31 22:41:54 +0900

    다한거 -> 플레이어 움직임, 카메라 트래킹 거의 다한거 -> 에니메이션, 맵 버그난 거 -> 다행이도 아직은 없음

---
+++ Project_R/Assets/Scripts/FollowSky.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowSky : MonoBehaviour
+{
+    private new PlayerMove gameObject = null;
+
+
+
+
+    void Start()
+    {
+        gameObject = FindObjectOfType<PlayerMove>();
+    }
+
+
+    void Update()
+    {
+        transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, 11);
+    }
+}
---
+++ Project_R/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float fPlayerSpeed = 5f;
+    [SerializeField] private float fJumpForce = 5f;
+
+    private Rigidbody2D rigidBody = null;
+    private Animator animator = null;
+    private SpriteRenderer spriteRenderer = null;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+        IdlePlayer();
+        StartCoroutine(JumpPlayer());
+    }
+
+
+
+    private void IdlePlayer()
+    {
+        // TODO : 여기 좀 많이 더티함
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            animator.Play("PlayerIdle");
+        }
+    }
+
+    private void MovePlayer()
+    {
+        float movement = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * fPlayerSpeed;
+        transform.rotation = Quaternion.identity;
+
+        // TODO : 여기 좀 더티함, 나아중에 입력 바꿔야 함
+        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            animator.Play("PlayerMove");
+            spriteRenderer.flipX = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            animator.Play("PlayerMove");
+            spriteRenderer.flipX = false;
+            
+        }
+    }
+
+    IEnumerator JumpPlayer()
+    {
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rigidBody.velocity.y) < 0.001f)
+        {
+            animator.Play("PlayerJump");
+            rigidBody.AddForce(new Vector2(0f, fJumpForce), ForceMode2D.Impulse);
+            yield return new WaitForSeconds(0.3f);
+            animator.Play("PlayerIdle");
+        }
+    }
+}
+
+#region 우엽이가 공부하면서 써둔 주석들 모음집
+/*
+ForceMode2D.Impulse
+// 폭팔, 충돌과 같은 퍙! 하고 짧은 시간에 운동에너지 크기를 키우는 경우
+Impluse
+//충격
+
+Mathf.Abs(float f);
+//f 의 절대값을 반환함
+
+Quaternion.identity;
+// 유니티는 내부적으로 회전을 Quaternion 으로 저장
+// identity 는 없음을 의미
+
+
+*/
+#endregion
