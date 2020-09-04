d1f8f14ae4b527eb4e19147e8ed5ff3f2e66f9bf
Gunuk <ggmrjsdnr@gmail.com>
2020-09-04 13:43:10 +0900

    아몰라 난 걍 라이브러리 올림

---
+++ Project_R/Assets/Scripts/UI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void OnClickHomeButton()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
