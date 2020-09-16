138eaa3e37fb86c2ca4f774996eb01add55ccdc5
Gunuk <ggmrjsdnr@gmail.com>
2020-09-16 19:31:01 +0900

    HP 바

--- Project_R/Assets/Scripts/Tutorial/Player/Attack/PlayerHP.cs
+++ Project_R/Assets/Scripts/Tutorial/Player/Attack/PlayerHP.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
-    [SerializeField] private int iPlayerHP = 100;
+    [SerializeField]
+    private int maxPlayerHP = 100;
+    [SerializeField] 
+    private int iPlayerHP = 100;
+
+    
+    [SerializeField]
+    private Slider slider;
 
 
     public void DecreaseHealth(int collisionDamage)
@@ -13,7 +21,12 @@ public class PlayerHP : MonoBehaviour
         iPlayerHP -= collisionDamage;
     }
 
-    
+    private void Start()
+    {
+        slider.value = (float)iPlayerHP / (float)maxPlayerHP;
+    }
+
+
     void Update()
     {
         if(iPlayerHP <= 0)
@@ -22,5 +35,15 @@ public class PlayerHP : MonoBehaviour
             Debug.Log("HP reset to 100");
             iPlayerHP = 100;
         }
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            iPlayerHP -= 10;
+        }
+        HandleHP();
     }
+    void HandleHP() 
+    {
+        slider.value = (float)iPlayerHP / (float)maxPlayerHP;
+    }
+
 }
