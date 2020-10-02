1dc422094f67cdbb39402da9f4cd28d9124ab774
Gunuk <ggmrjsdnr@gmail.com>
2020-10-03 08:19:33 +0900

    모노싱글톤 만들어 놓음 쓰는 법은 인터넷 참조

--- Project_R/Assets/Scripts/Tutorial/GameManager.cs
+++ Project_R/Assets/Scripts/Tutorial/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoSingleton<GameManager>
 {
     public bool bAtJump = false;
     public float fGroundLevel = -0.19f;
