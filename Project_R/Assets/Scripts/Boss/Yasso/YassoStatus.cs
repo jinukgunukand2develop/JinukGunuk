1957488d0fc5a739e306c5bcbcdd4135c9489fdf
devblue_ <wooyeophan@gmail.com>
2020-10-29 22:58:17 +0900

    / 드디어 버튼으로 움직일 수 있습니다. / 점프까지 구현된 참된 버튼. / 의도치않게 미끄러운 움직임도 해결함
    / 보스전 플레이어 피격 데미지 적용
    
    // 야스오 스킬
    / 야스오 Q 만듬
    
    // 야스오 AI :
    / 등장후 공격, 빠지기 판단 (시간이 된다면 이 둘이 적극적, 소극적 보스 패턴을 나눌 것)
    / 랜덤으로 스킬 사용 (연달아 많이 사용한 경우 확률 하락)
    / 야스오가 대쉬 중일때 공격을 한다면 EQ를 사용함 (이것 또한 확률)
    / 플레이어를 탐지해 자신의 다음 행동을 랜덤으로 정함
    / 풀래이어가 탐지되지 않았다면 일정 시간 idle 후 무작위 위치로 점프함
    / 연속된 idle 상태 횟수와 idle 대기 시간은 반비례함
    
    / YassoAI 클래스 이름에 마우스를 올리면 설명이 나와요

--- Project_R/Assets/Scripts/Boss/Yasso/YassoStatus.cs
+++ Project_R/Assets/Scripts/Boss/Yasso/YassoStatus.cs
@@ -6,11 +6,13 @@ public class YassoStatus : MonoBehaviour
 {
     public short iHP = 500;
     public byte skillUseCount = 0;
+    public byte beenIdleFor = 0;
 
     public bool bJumping = false;
     public bool bIsDash = false;
     public bool bUsedQ = false;
     public bool bUsedW = false;
     public bool bUsedE = false;
+    public bool bDashAttacked = false;
     public bool bAbleR = false;
 }
