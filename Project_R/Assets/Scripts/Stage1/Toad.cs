3552d2194cbeb81d2f583a12d2466cabab70565a
DESKTOP-GQ8UI5N\ggmrj <ggmrjsdnr@gmail.com>
2020-11-17 22:04:01 +0900

    hfghfghfghfghfhg

---
+++ Project_R/Assets/Scripts/Stage1/Toad.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toad :CToad
+{
+    private bool bAttackDelay = true;
+
+    [SerializeField] private GameObject player = null;
+    [SerializeField] private float fCollisionDamageDelay = 1f;
+
+    private SpriteRenderer sprite = null;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        StartCoroutine(CheckCollision());
+        if (iHp <= 0)
+            Destroy(gameObject);
+    }
+
+    private IEnumerator CheckCollision()
+    {
+        if (bAttackDelay && Vector2.Distance(player.transform.position, transform.position) <= 0.1f)
+        {
+            bAttackDelay = false;
+            FindObjectOfType<GameManager>().SendMessage("DecreaseHealth", collisionDamage, SendMessageOptions.DontRequireReceiver);
+            yield return new WaitForSeconds(fCollisionDamageDelay);
+            bAttackDelay = true;
+        }
+    }
+
+    public void ZrE()
+    {
+        StartCoroutine(Damaged());
+        iHp -= 10;
+    }
+    public void ZrQ()
+    {
+        StartCoroutine(Damaged());
+        iHp -= 5;
+    }
+    public void SzW()
+    {
+        StartCoroutine(Damaged());
+        iHp -= 10;
+    }
+    public void Hit()
+    {
+        iHp -= 5;
+    }
+
+    private IEnumerator Damaged()
+    {
+        sprite.color = new Color32(255, 255, 255, 90);
+        yield return new WaitForSeconds(0.1f);
+        sprite.color = new Color32(255, 255, 255, 255);
+    }
+
+    protected void Dead()
+    {
+        Destroy(gameObject);
+    }
+}
