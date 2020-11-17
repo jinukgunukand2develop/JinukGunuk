85bcd1251c39d74eb8f984c70f1be431b01dfc7b
seoseonho <wahybzb24@gmail.com>
2020-11-17 14:41:40 +0900

    배경 픽셀퍼펙트 깨진거 고쳐드렷음

---
+++ Project_R/Assets/Scripts/backgroundmove_tuto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class backgroundmove_tuto : MonoBehaviour
+{
+    [SerializeField]
+
+    private float backg_speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * backg_speed); ;
+    }
+}
